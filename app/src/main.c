f4bcce5e619abb2dd62b8de1279b14f122281c21
Benjamin Cabé <benjamin@zephyrproject.org>
2024-03-29 18:31:24 +0100

    update to Zephyr 3.6
    also make sample app more minimal (i.e. no dependency on nrf)
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- app/src/main.c
+++ app/src/main.c
@@ -7,31 +7,7 @@
 #define MODULE main
 
 #include <zephyr/logging/log.h>
-#include <caf/events/module_state_event.h>
-#include <zephyr/settings/settings.h>
 
 LOG_MODULE_REGISTER(MODULE);
 
-int main(void)
-{
-    /* Start CAF and signal that main have completed system setup */
-    if (app_event_manager_init()) {
-        LOG_ERR("Application Event Manager not initialized!");
-        return 1;
-    }
-
-    settings_subsys_init();
-    settings_load();
-
-    LOG_DBG("Event manager initialized.");
-    module_set_state(MODULE_STATE_READY);
-
-    return 0;
-}
-
-/**
- * Local Variables:
- *  indent-tabs-mode: nil
- *  c-file-style: "stroustrup"
- * End:
- */
+int main(void) { return 0; }
